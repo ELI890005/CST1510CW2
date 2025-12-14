f086542533f666ac330e188f086bd52a2e4e9a0b
Elijah <takeoffeli93@gmail.com>
2025-12-14 14:23:31 +0300

    Updated dashboards and data

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from database import db
 
-st.set_page_config(page_title="Multi Domain Intelligence Platform", page_icon="🧠")
+st.set_page_config(page_title="Multi Domain Intelligence Platform", page_icon="")
 
-st.title("🧠 Multi-Domain Intelligence Platform")
+st.title(" Multi-Domain Intelligence Platform")
 st.write("Login or Register using the sidebar.")
 
 st.sidebar.success("Choose a page to begin.")
